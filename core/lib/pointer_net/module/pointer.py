aebb7380b58ad5ba6fa045c90de0adc4d31afbe9
hocdot <duongvanhai187@gmail.com>
2025-06-13 17:13:37 -0400

    x

--- core/lib/pointer_net/module/pointer.py
+++ core/lib/pointer_net/module/pointer.py
@@ -9,22 +9,22 @@ class Pointer(nn.Module):
         super().__init__()
         self.args = args
         self.v = nn.Parameter(torch.FloatTensor(args.n_hidden).to(args.device))
-        self.W_ref = nn.Conv1d(args.n_hidden, args.n_hidden, 1, 1,
-                               device=args.device)
-        self.W_q = nn.Linear(args.n_hidden, args.n_hidden,
-                             bias=True, device=args.device)
+        self.W_ref = nn.Conv1d(args.n_hidden, args.n_hidden, 1, 1, device=args.device)
+        self.W_q = nn.Linear(args.n_hidden, args.n_hidden, bias=True, device=args.device)
 
     @beartype
     def forward(self, q: torch.Tensor, ref: PackedSequence, mask: torch.Tensor, inf: float = 1e8) -> torch.Tensor:
         args = self.args
-        # ref: (bs, n_node, n_hidden)
-        ref = torch.nn.utils.rnn.pad_packed_sequence(ref)[0].permute(1, 0, 2)
+        # ref: (bs, n_hidden, n_node)
+        padded_ref = torch.nn.utils.rnn.pad_packed_sequence(ref)[0].permute(1, 2, 0)
+        bs, _, n_node = padded_ref.shape
         # u1: (bs, n_hidden, n_node)
-        u1 = self.W_q(q).unsqueeze(-1).repeat(1, 1, ref.size(1))
+        u1 = self.W_q(q).unsqueeze(-1).repeat(1, 1, n_node)
         # u2: (bs, n_hidden, n_node)
-        u2 = self.W_ref(ref.permute(0, 2, 1))
-        V = self.v.unsqueeze(0).unsqueeze(0).repeat(ref.size(0), 1, 1)
+        u2 = self.W_ref(padded_ref)
+        # vs: (bs, 1, n_hidden)
+        vs = self.v.unsqueeze(0).unsqueeze(0).repeat(bs, 1, 1).to(args.device)
         # u: (bs, n_node)
-        u = torch.bmm(V, args.clip_logit * torch.tanh(u1 + u2)).squeeze(1)
+        u = torch.bmm(vs, args.clip_logit * torch.tanh(u1 + u2)).squeeze(1)
         u = u - inf * mask
         return u
